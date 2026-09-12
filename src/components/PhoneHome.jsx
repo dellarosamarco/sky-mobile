6fc33a2c4149182a171743f30889785b41310e65
Marco <96957446+dellarosamarco@users.noreply.github.com>
2026-09-12 22:40:22 +0200

    Add X Factor to phone home

--- src/components/PhoneHome.jsx
+++ src/components/PhoneHome.jsx
@@ -12,6 +12,7 @@ const APPS = [
   { id: 'spotify', label: 'Spotify', icon: '/app-icons/spotify.jpg' },
   { id: 'facetime', label: 'FaceTime', icon: '/app-icons/facetime.jpg' },
   { id: 'teams', label: 'Teams', icon: '/app-icons/teams.jpg' },
+  { id: 'xfactor', label: 'X Factor', icon: '/app-icons/xfactor.jpg' },
   { id: 'mail', label: 'Mail', icon: '/app-icons/mail.jpg' },
   { id: 'google', label: 'Google', icon: '/app-icons/google.jpg' },
   { id: 'sky', label: 'Sky Mobile' },
