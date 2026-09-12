50a67f2171a87b0ad11bad9a9ba33c10f7198b71
Marco <96957446+dellarosamarco@users.noreply.github.com>
2026-09-12 22:38:04 +0200

    Add FaceTime to iPhone home

--- src/components/PhoneHome.jsx
+++ src/components/PhoneHome.jsx
@@ -10,6 +10,7 @@ const APPS = [
   { id: 'whatsapp', label: 'WhatsApp', icon: '/app-icons/whatsapp.jpg' },
   { id: 'instagram', label: 'Instagram', icon: '/app-icons/instagram.jpg' },
   { id: 'spotify', label: 'Spotify', icon: '/app-icons/spotify.jpg' },
+  { id: 'facetime', label: 'FaceTime', icon: '/app-icons/facetime.jpg' },
   { id: 'mail', label: 'Mail', icon: '/app-icons/mail.jpg' },
   { id: 'google', label: 'Google', icon: '/app-icons/google.jpg' },
   { id: 'sky', label: 'Sky Mobile' },
