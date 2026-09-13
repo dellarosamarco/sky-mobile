dec81f9c1f6c4e9ee4a64fcb45bebadd09efd7fa
Marco <96957446+dellarosamarco@users.noreply.github.com>
2026-09-13 12:01:47 +0200

    fix: map My Sky and My Sodexo icons to correct references

--- src/components/PhoneHome.jsx
+++ src/components/PhoneHome.jsx
@@ -13,7 +13,7 @@ const APPS = [
   { id: 'facetime', label: 'FaceTime', icon: '/app-icons/facetime.jpg' },
   { id: 'teams', label: 'Teams', icon: '/app-icons/teams.jpg' },
   { id: 'xfactor', label: 'X Factor', icon: '/app-icons/xfactor.jpg' },
-  { id: 'mysky', label: 'My Sky', icon: '/app-icons/mysky.svg' },
+  { id: 'mysky', label: 'My Sky', icon: '/app-icons/mysky.jpg' },
   { id: 'skytoday', label: 'Sky Today', icon: '/app-icons/skytoday.svg' },
   { id: 'youtube', label: 'YouTube', icon: '/app-icons/youtube.jpg' },
   { id: 'mysodexo', label: 'My Sodexo', icon: '/app-icons/mysodexo.svg' },
