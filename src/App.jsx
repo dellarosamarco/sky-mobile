539903a34710a3175f163e6ca5dd88531d1226c4
Marco <96957446+dellarosamarco@users.noreply.github.com>
2026-09-16 19:29:05 +0200

    feat: add game2 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import GamePage from './pages/GamePage'
+import Game2Page from './pages/Game2Page'
 import VideoCallPage from './pages/VideoCallPage'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/game" replace />} />
       <Route path="/game" element={<GamePage />} />
+      <Route path="/game2" element={<Game2Page />} />
       <Route path="/videocall" element={<VideoCallPage />} />
       <Route path="*" element={<Navigate to="/game" replace />} />
     </Routes>
