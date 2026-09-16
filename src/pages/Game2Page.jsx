1564ce0d06494bf1c0ab91a7ff7c915caa485f01
Marco <96957446+dellarosamarco@users.noreply.github.com>
2026-09-16 19:28:55 +0200

    feat: add game2 page

---
+++ src/pages/Game2Page.jsx
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react'
+import IPhoneLockScreen from '../components/IPhoneLockScreen'
+import PhoneHome from '../components/PhoneHome'
+import GameExperience2 from '../components/GameExperience2'
+
+export default function Game2Page() {
+  const [phase, setPhase] = useState('locked')
+  const reset = useCallback(() => setPhase('locked'), [])
+
+  if (phase === 'locked') {
+    return <IPhoneLockScreen pin="1234" onUnlock={() => setPhase('home')} />
+  }
+
+  if (phase === 'home') {
+    return <PhoneHome mode="game" onOpen={() => setPhase('game')} />
+  }
+
+  return <GameExperience2 onReset={reset} />
+}
